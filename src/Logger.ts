8363bdf7619c88962fdda34a67ff8f4472e92326
Naya <44099724+nayamairena@users.noreply.github.com>
2022-02-27 23:54:34 -0800

    Need to document last two methods

--- src/Logger.ts
+++ src/Logger.ts
@@ -25,7 +25,8 @@
  * The Logger class allows for specified console logs
  * to display messages to the console by using variables of class type Logger.
  *
- *- The four levels of severity held in **properties**:
+ *- The four levels of severity held in **properties**
+ * and are protected to only allow manipulation by class **methods**:
  *      - `logDebug`
  *      - `logError`
  *      - `logInfo`
@@ -49,11 +50,56 @@
  *
  */
 export class Logger {
+    /**
+     * Protected property logError to keep track if enabled or disabled.
+     * ```
+     * // logError definition for Logger class.
+     * protected logError: number;
+     * ```
+     */
     protected logError: number;
+    /**
+     * Protected property logWarn to keep track if enabled or disabled.
+     * ```
+     * // logWarn definition for Logger class.
+     * protected logWarn: number;
+     * ```
+     */
     protected logWarn: number;
+    /**
+     * Protected property logInfo to keep track if enabled or disabled.
+     * ```
+     * // logInfo definition for Logger class.
+     * protected logInfo: number;
+     * ```
+     */
     protected logInfo: number;
+    /**
+     * Protected property logDebug to keep track if enabled or disabled.
+     * ```
+     * // logDebug definition for Logger class.
+     * protected logDebug: number;
+     * ```
+     */
     protected logDebug: number;
 
+    /**
+     * Logger class constructor to initialize **properties** to 1.
+     * ```
+     * // Constructor definition for Logger class.
+     * constructor() {
+     *   this.logError = 1;
+     *   this.logWarn = 1;
+     *   this.logInfo = 1;
+     *   this.logDebug = 1;
+     * }
+     * ```
+     * @usage
+     * ```
+     * // Create new instance of Logger class type.
+     * const log = new Logger();
+     * ```
+     */
     constructor() {
         this.logError = 1;
         this.logWarn = 1;
@@ -61,6 +107,36 @@ export class Logger {
         this.logDebug = 1;
     }
 
+    /**
+    * This **method** allows for a severity level 
+    * to send a message to the console.
+    * Also checks if it is enabled or disabled.
+    *
+    * ```
+    * // consoleOutput definition for Logger class.
+    * consoleOutput(logNum: number, message: string): void {
+    *   if (logNum < 1 || logNum > 4)
+    *      console.error("Console type does not exist. Use a number 1 - 4.");
+    *   else if (logNum === 1 && this.logError === 1)
+    *       console.error(message);
+    *  else if (logNum === 2 && this.logWarn === 1)
+    *     console.warn(message);
+    *  else if (logNum === 3 && this.logInfo === 1)
+    *      console.info(message);
+    *  else if (logNum === 4 && this.logDebug === 1)
+    *    console.debug(message);
+    }
+    * }
+    * ```
+    * @usage
+    * ```
+    * // Call the consoleOutput method example.
+    * log.consoleOutput(1, message);
+    * ```
+    * @param logNum
+    * *logNum can only be a number from [1 - 4].*
+    * @param message
+    */
     consoleOutput(logNum: number, message: string): void {
         if (logNum < 1 || logNum > 4)
             console.error("Console type does not exist. Use a number 1 - 4.");
